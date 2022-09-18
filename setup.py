cef3878fd614131f8b4cdd745bdce393a63b1241
bookla <booklailert@gmail.com>
2022-09-18 13:24:47 +0100

    Updated version number after previous changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='DynamoDBInterface',
-   version='1.0.0',
+   version='1.0.1',
    description='Python Interface to Streamline Access to DynamoDB database tables.',
    long_description=long_description,
    author='Pakkapol Lailert',
